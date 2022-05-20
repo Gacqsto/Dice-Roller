be99ad8e1436feaf9df53b0aebf8c24b48ef3925
Gianni Acquisto <gianni.acquisto@gmail.com>
2022-05-20 23:56:32 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll Boomer"
+
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "Button clicked by a boomer!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
